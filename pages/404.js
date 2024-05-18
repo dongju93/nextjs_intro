6d8997b99eb240e5f837d7679e32cc15b54663a5
dongju93 <spdlqj011@gmail.com>
2024-05-19 01:42:20 +0900

    :adhesive_bandage: replace unescaped single quotes

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,7 @@
 export default function error404Page() {
     return (
-    <div>
-        <h4>Don't even think about it</h4>
-    </div>
-)};
+        <div>
+            <h4>Don&apos;t even think about it</h4>
+        </div>
+    );
+}
